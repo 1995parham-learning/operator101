93b246ad57f4b8b546058b5ec787c538af143d55
Parham Alvani <parham.alvani@gmail.com>
2022-11-20 17:18:22 +0330

    feat: initialize project with a single api

---
+++ api/v1alpha1/helloer_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// HelloerSpec defines the desired state of Helloer
+type HelloerSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of Helloer. Edit helloer_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// HelloerStatus defines the observed state of Helloer
+type HelloerStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Helloer is the Schema for the helloers API
+type Helloer struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   HelloerSpec   `json:"spec,omitempty"`
+	Status HelloerStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// HelloerList contains a list of Helloer
+type HelloerList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Helloer `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Helloer{}, &HelloerList{})
+}
