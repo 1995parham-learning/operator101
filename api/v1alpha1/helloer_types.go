6dbbcdf7e24d5466b57337b948a3febfc126a615
Parham Alvani <parham.alvani@gmail.com>
2023-11-25 07:38:00 +0000

    feat: update helloer manifest

--- api/v1alpha1/helloer_types.go
+++ api/v1alpha1/helloer_types.go
@@ -20,7 +20,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// EDIT THIS FILE! THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // HelloerSpec defines the desired state of Helloer
@@ -28,8 +28,8 @@ type HelloerSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Helloer. Edit helloer_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// +operator-sdk:csv:customresourcedefinitions:type=spec
+	To string `json:"to,omitempty"`
 }
 
 // HelloerStatus defines the observed state of Helloer
